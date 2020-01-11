287f2d6f605383bc3651d8f76fc899b88e3b030d
FMarianteCardenas <fmariantecar@gmail.com>
2020-01-11 13:03:05 -0300

    agregado de vuex-persistedstate para persistir los datos al recargar la pagina

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+//plugin para persistir los datos de vuex
+import createPersistedState from 'vuex-persistedstate'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    base_url:'',
+    usuario:null,
+    roles:null,
+    autenticado:false,
+    headers:null,
   },
   mutations: {
   },
   actions: {
   },
   modules: {
-  }
+  },
+  plugins:[
+    createPersistedState({
+      paths: ['base_url','usuario','roles','autenticado','headers']
+    })
+  ]
 })
